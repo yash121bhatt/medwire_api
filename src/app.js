982e07249c6df1db8f0fdb70f3e77ef9454dfb94
Rohit Kumar Mahor <rk85783@gmail.com>
2025-11-10 18:28:20 +0530

    add test mail code

--- src/app.js
+++ src/app.js
@@ -21,12 +21,54 @@ const path = require("path");
 const helperFunction = require("./helper/helperFunction");
 app.use(express.static("public"));
 
+const nodemailer = require("nodemailer");
+app.get("/send-email", async (req, res) => {
+    console.info(req.method, req.originalUrl);
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.sendgrid.net",
+            // port: 587,
+            // secure: false,
+            // port: 587,
+            // secure: false,
+            port: 2525,
+            secure: false,
+            auth: {
+                user: "apikey",
+                pass: "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",
+            },
+        });
+
+        const to = req.query?.to || "rk85783@mailinator.com";
+        const info = await transporter.sendMail({
+            from: "yash121bhatt@gmail.com",
+            to,
+            subject: "✅ Test Email from SendGrid + Nodemailer",
+            html: "<h2>It works! 🚀</h2><p>This is a test email sent via SendGrid SMTP and Nodemailer.</p>",
+        });
+
+        console.log("✅ Email sent successfully:", info.messageId);
+
+        res.status(200).json({
+            success: true,
+            message: `Mail sent to : ${to}`
+        });
+    } catch (error) {
+        console.error("❌ Failed to send email:", error);
+        res.status(500).json({
+            success: true,
+            message: "Internal server error",
+            stack: error.stack
+        });
+    }
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/laboratory", laboratoryRoute);
 
 app.get("/", (req, res) => {
-    
+
     // let email_id = 'rk85783@mailinator.com'
 
     // Example with templete
